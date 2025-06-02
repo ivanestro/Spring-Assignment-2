027ddd37f220813f576442d318baffb0c01017d4
Ivan Estropigan <iestropigan@rrc.ca>
2025-06-01 22:37:52 -0500

    Update: Question 1, 2, and 3 are complete for Simple Data Types.

--- estropigan_i_assignment_2.py
+++ estropigan_i_assignment_2.py
@@ -1,5 +1,22 @@
-"""Assignment 2 """
+"""Assignment 2."""
 
 __author__ = "Ivan Estropigan"
 __version__ = "1.0.0"
 __credits__ = "COMP-1327 Dev Team"
+
+## SIMPLE DATA TYPES
+
+# Variables 
+NAME = 'Ivan'
+MANITOBA_DRIVERS_LICENSE = False
+
+# Print name and datatype
+print("name: ", NAME, "type:", type(NAME))
+
+# 
+MANITOBA_DRIVERS_LICENSE = input("Do you have Drivers License? [True] or [False] ")
+if MANITOBA_DRIVERS_LICENSE == 'True':
+    print("has license: {MANITOBA_DRIVERS_LICENSE}", "type: ", type(MANITOBA_DRIVERS_LICENSE))
+
+elif MANITOBA_DRIVERS_LICENSE == 'False':
+    print("Invalid Drivers License.")
